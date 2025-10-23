5801830350e54da482b19b0e67242744904968e5
danhumassmed <daniel.higgins@umassmed.edu>
2025-10-23 10:58:02 -0400

    updates to crud

--- src/pg_scaffold/generator/v2/templates/crud_base.py
+++ src/pg_scaffold/generator/v2/templates/crud_base.py
@@ -6,40 +6,56 @@ from app.models.base import Base
 
 ModelType = TypeVar("ModelType", bound=Base)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+ReadSchemaType = TypeVar("ReadSchemaType", bound=BaseModel)
 UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
 
 
-class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
-    def __init__(self, model: Type[ModelType]):
+class CRUDBase(Generic[ModelType, CreateSchemaType, ReadSchemaType, UpdateSchemaType]):
+    def __init__(self, model: Type[ModelType], 
+                 CreateSchema: Type[CreateSchemaType],
+                 ReadSchema: Type[ReadSchemaType],
+                 UpdateSchema: Type[UpdateSchemaType],
+                 db: Session):
         self.model = model
+        self.CreateSchema = CreateSchema
+        self.ReadSchema = ReadSchema
+        self.UpdateSchema = UpdateSchema
+        self.db = db
+
+    # Hook methods — override in subclass if needed
+    def _get_first_hook(self, query: Query) -> Query:
+        return query
+
+    def _get_many_hook(self, query: Query) -> Query:
+        return query
+
+    def _get_many_like_hook(self, query: Query) -> Query:
+        return query
 
     def _create_validation_hook(self):
         return True
     
-    # Hook methods — override in subclass if needed
-    def _apply_get_hook(self, query: Query) -> Query:
-        return query
+    def get_by_id(self,id: Any) -> Optional[ReadSchemaType]:
+        query = self.db.query(self.model)
+        query = self._get_first_hook(query)
+        db_obj = query.filter(self.model.id == id).first()
+        return self.ReadSchema.model_validate(db_obj)
+    
+    def get_many(self, *, skip: int = 0, limit: int = 100
+    ) -> List[ReadSchemaType]:
+        query = self.db.query(self.model)
+        query = self._get_many_hook(query)
+        db_objs = query.offset(skip).limit(limit).all()
+        return [self.ReadSchema.model_validate(db_obj) for db_obj in db_objs]
 
-    def _apply_filter_hook(self, query: Query) -> Query:
-        return query
-
-    def _apply_multi_hook(self, query: Query) -> Query:
-        return query
-
-    def get(self, db: Session, id: Any) -> Optional[ModelType]:
-        query = db.query(self.model)
-        query = self._apply_get_hook(query)
-        return query.filter(self.model.id == id).first()
-
-    def filter_multi_like(
+    def get_many_like(
         self,
-        db: Session,
         filters: Dict[str, str],
         skip: int = 0,
         limit: int = 100,
-    ) -> List[ModelType]:
-        query = db.query(self.model)
-        query = self._apply_filter_hook(query)
+    ) -> List[ReadSchemaType]:
+        query = self.db.query(self.model)
+        query = self._get_many_like_hook(query)
 
         for field, value in filters.items():
             column = getattr(self.model, field, None)
@@ -47,29 +63,24 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
                 raise ValueError(f"Invalid field name '{field}' for model {self.model.__name__}")
             query = query.filter(column.ilike(f"%{value}%"))
 
-        return query.offset(skip).limit(limit).all()
+        db_objs = query.offset(skip).limit(limit).all()
+        return [self.ReadSchema.model_validate(db_obj) for db_obj in db_objs]
 
-    def get_multi(
-        self, db: Session, *, skip: int = 0, limit: int = 100
-    ) -> List[ModelType]:
-        query = db.query(self.model)
-        query = self._apply_multi_hook(query)
-        return query.offset(skip).limit(limit).all()
-
-    def create(self, db: Session, *, obj_in: CreateSchemaType) -> Optional[ModelType]:
+    def create(self, obj_in: CreateSchemaType) -> Optional[ReadSchemaType]:
         db_obj = None
         valid_input = self._create_validation_hook()
         if valid_input:
             obj_in_data = jsonable_encoder(obj_in)
             db_obj = self.model(**obj_in_data)
-            db.add(db_obj)
-            db.commit()
-            db.refresh(db_obj)
-        return db_obj
+            self.db.add(db_obj)
+            self.db.commit()
+            self.db.refresh(db_obj)
+        return self.ReadSchema.model_validate(db_obj)
 
     def update(
-        self, db: Session, *, db_obj: ModelType, obj_in: Union[UpdateSchemaType, Dict[str, Any]]
-    ) -> ModelType:
+        self, obj_in: Union[UpdateSchemaType, Dict[str, Any]]
+    ) -> Optional[ReadSchemaType]:
+        db_obj = self.db.query(self.model).get(obj_in.id)
         obj_data = jsonable_encoder(db_obj)
         update_data = obj_in if isinstance(obj_in, dict) else obj_in.dict(exclude_unset=True)
 
@@ -77,13 +88,14 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
             if field in update_data:
                 setattr(db_obj, field, update_data[field])
 
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-        return db_obj
+        self.db.add(db_obj)
+        self.db.commit()
+        self.db.refresh(db_obj)
+        return self.ReadSchema.model_validate(db_obj)
 
-    def remove(self, db: Session, *, id: int) -> ModelType:
-        obj = db.query(self.model).get(id)
-        db.delete(obj)
-        db.commit()
-        return obj
+    def remove(self, *, id: int) -> Optional[ReadSchemaType]:
+        #db_obj = self.db.query(self.model).get(id)
+        db_obj = self.db.get(self.model, id)
+        self.db.delete(db_obj)
+        self.db.commit()
+        return self.ReadSchema.model_validate(db_obj)
